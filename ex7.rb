67ed81511f927d12792ec2d68dee7764a199d0a0
David T <kenshin777>
2012-06-20 18:55:58 +0200

    exercise 7

---
+++ ex7.rb
@@ -0,0 +1,25 @@
+puts "Mary had a little lamb."
+puts "Its fleece was wite as %s." % 'snow'
+puts "And everywhere that mary went."
+puts "." * 10 # what'd that do?
+
+end1 ="C"
+end2 ="h"
+end3 ="e"
+end4 ="e"
+end5 ="s"
+end6 ="e"
+end7 ="b"
+end8 ="u"
+end9 ="r"
+end10 ="g"
+end11 ="e"
+end12 ="r"
+
+#note how we are using print insted of puts where. change it to puts and see what happens
+
+print end1 + end2+ end3+end4+end5+end6
+print end7 + end8+ end9 + end10 + end11 + end12
+
+#this just is polite use of the terminal, try removing it
+puts

b98024800d407799159b67fb367637a730f91a64
David T <kenshin777>
2012-07-11 21:45:14 +0200

    do lexicon pass test

--- lexicon/lib/lexicon.rb
+++ lexicon/lib/lexicon.rb
@@ -1,5 +1,35 @@
-require "lexicon/version"
+#require "lexicon/version"
 
-module Lexicon
-  # Your code goes here...
+class LexiconScan
+
+  Pair = Struct.new(:token,:word)
+  
+  def initialize
+    
+    @words = %w[north south east west down up left right back]
+    @verbs = %w[go stop kill eat]
+    @stop_words = %w[the in of from at it]
+    @nouns = %w[door bear princess cabinet]
+    @number_pattern = "[0-9]+"
+    
+  end
+
+  def scan(input)
+    
+    sentence = []
+
+    words = input.split
+    
+    words.each do |word|
+      puts word
+      puts @words[0]
+#      puts @words.include(word)
+      
+      sentence.push(Pair.new(:direction,word)) if (@words.include?(word))
+      
+    end
+    
+    sentence
+  end
+
 end

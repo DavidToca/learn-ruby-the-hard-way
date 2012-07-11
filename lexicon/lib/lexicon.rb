59a0341f4112fe195a4fa993cb060b4d53b81b8c
David T <kenshin777>
2012-07-12 01:09:56 +0200

    @WIP

--- lexicon/lib/lexicon.rb
+++ lexicon/lib/lexicon.rb
@@ -14,19 +14,25 @@ class LexiconScan
     
   end
 
+
   def scan(input)
     
     sentence = []
 
     words = input.split
+
+
     
     words.each do |word|
-      puts word
-      puts @words[0]
-#      puts @words.include(word)
-      
-      sentence.push(Pair.new(:direction,word)) if (@words.include?(word))
-      
+
+     sentence.push(Pair.new(:direction,word)) if @words.include? word
+     sentence.push(Pair.new(:verb,word)) if @verbs.include? word
+     sentence.push(Pair.new(:stop,word)) if @stop_words.include? word
+     sentence.push(Pair.new(:noun,word)) if @nouns.include? word
+     sentence.push(Pair.new(:number,word.to_i)) if word.match(@number_pattern)
+     
+     
+
     end
     
     sentence
--- lexicon/test/lexicon_rspec.rb
+++ lexicon/test/lexicon_rspec.rb
@@ -1,12 +1,10 @@
 require  '../lib/lexicon'
 
-#require "./lexicon.rb"
-
 describe LexiconScan do
 
-  before :each do
+  before :all do
     @lexicon = LexiconScan.new
-    Pair = LexiconScan::Pair
+    Pair = LexiconScan::Pair 
   end
 
   it "should parse all directions" do
@@ -27,6 +25,101 @@ describe LexiconScan do
 
   end
 
+  it "should parse al verbs" do
+    
+    go  = Pair.new :verb, 'go'
+    
+    kill = Pair.new :verb, 'kill'
+
+    eat = Pair.new :verb, 'eat'
+
+    go_kill_eat = [go,kill,eat]
+    
+    result_scan = @lexicon.scan("go")
+
+    result_scan.should == [go]
+
+    result_scan = @lexicon.scan("go kill eat")
+
+    result_scan.should == go_kill_eat
+
+    
+  end
+
+  it "should parse stops" do
+    
+    the = Pair.new(:stop, 'the')
+    in_stop = Pair.new(:stop, 'in')
+    of = Pair.new(:stop,'of')
+    
+    the_in_of = [the, in_stop, of]
+
+    result_scan = @lexicon.scan("the")
+
+    result_scan.should == [the]
+
+    result_scan = @lexicon.scan("the in of")
+
+    result_scan ==  the_in_of
+    
+  end
+
+  it "should parse all nouns" do
+    
+    bear = Pair.new(:noun, 'bear')
+
+    princess = Pair.new(:noun,'princess')
+
+    bear_princess = [bear, princess]
+
+    result_scan = @lexicon.scan("bear")
+
+    result_scan.should == [bear]
+
+    result_scan = @lexicon.scan("bear princess")
+
+    result_scan.should == bear_princess
+
+  end
+
+  it "should parse all numbers" do
+
+    number1 = Pair.new(:number,91234)
+    number2 = Pair.new(:number,3)
+    
+    numbers = [number2, number1]
+
+    result_scan = @lexicon.scan("91234")
+
+    result_scan.should == [number1]
+
+    result_scan = @lexicon.scan("3 91234")
+    
+    result_scan.should == numbers
+
+  end
+
+  it "should parse to error words that are not in direction, verbs, stop words, nouns and numbers" do
+    
+    ias  = Pair.new(:error,'IAS')
+
+    bear = Pair.new(:noun,'bear')
+
+    princess = Pair.new(:noun,'princess')
+
+    sentence = [ bear, ias, princess]
+    
+    result_scan = @lexicon.scan("ASDASDASD")
+
+    result_scan.should == [Pair.new(:error, 'ASDASDASD')]
+
+    result_scan = @lexicon.scan("bear IAS princess")
+
+    result_scan.should == sentence
+
+  end
+
+
+  
 
 end
-

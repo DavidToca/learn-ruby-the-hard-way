d9ba240ed90dc9bc1330d24c5148a0664ce8e391
David Toca <david.virusfel@gmail.com>
2012-07-11 21:26:05 -0500

    modify lexicon to pass all test

--- lexicon/lib/lexicon.rb
+++ lexicon/lib/lexicon.rb
@@ -1,40 +1,42 @@
-#require "lexicon/version"
-
 class LexiconScan
 
   Pair = Struct.new(:token,:word)
-  
+
   def initialize
-    
+
     @words = %w[north south east west down up left right back]
     @verbs = %w[go stop kill eat]
     @stop_words = %w[the in of from at it]
     @nouns = %w[door bear princess cabinet]
     @number_pattern = "[0-9]+"
-    
+
   end
 
-
   def scan(input)
-    
+
     sentence = []
 
     words = input.split
 
-
-    
     words.each do |word|
 
-     sentence.push(Pair.new(:direction,word)) if @words.include? word
-     sentence.push(Pair.new(:verb,word)) if @verbs.include? word
-     sentence.push(Pair.new(:stop,word)) if @stop_words.include? word
-     sentence.push(Pair.new(:noun,word)) if @nouns.include? word
-     sentence.push(Pair.new(:number,word.to_i)) if word.match(@number_pattern)
-     
-     
+      word_d  = word.downcase
 
+    if @words.include? word_d
+       sentence.push(Pair.new(:direction,word))
+    elsif @verbs.include? word_d
+      sentence.push(Pair.new(:verb,word))
+    elsif @stop_words.include? word_d
+     sentence.push(Pair.new(:stop,word))
+    elsif @nouns.include? word_d
+     sentence.push(Pair.new(:noun,word))
+    elsif word.match(@number_pattern)
+     sentence.push(Pair.new(:number,word.to_i))
+    else
+     sentence.push(Pair.new(:error,word))
     end
-    
+   end
+
     sentence
   end
 
